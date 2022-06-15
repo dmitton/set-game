30403d25fc6bdeefc47e70d62edf76466a1afdd6
David Mitton <davidbmitton@gmail.com>
2022-06-14 18:47:46 -0800

    Layout plan

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -12,20 +12,27 @@ export default {
   data() {
    return {
      gameArray: [],
+     cardsSelected: [],
    }
   },
   methods: {
     //start the game
     startGame(){
       //get four cards and put them in the game card list
-      this.getFourCards();
+      this.getGameCards();
       //get matches to each of those four cards in the list
       this.putMatchestoCards();
+
+      //TODO check if selected cards are a match
+
     },
-    getFourCards(){
+    getGameCards(){
       for (let i = 0; i < 12; i++){
         //get a random index
         var index = Math.floor(Math.random()*81);
+
+        //TODO check to make sure the card isn't grabbed
+        
         //push it onto the game card list
         this.gameArray.push(this.$root.$data.cards[index]);
       }
@@ -34,6 +41,17 @@ export default {
     },
     putMatchestoCards(){
        
+    },
+
+    //TODO cardClickedCounter
+    cardClickedCounter(){
+      //check how many cards are clicked
+      //if 3 cards are selected return true
+    },
+
+    //TODO selected cards are a match
+    isSet(){
+
     }
   }
 
